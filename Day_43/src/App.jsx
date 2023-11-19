b3af577604d709ea543999bc09b6abcc5a4a06c3
TranQuy252 <137138673+TranQuy252@users.noreply.github.com>
2023-11-19 14:00:42 +0700

    Add files via upload

---
+++ Day_43/src/App.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import ProductList from "./Components/Product/ProductList";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Cart from "./Components/Cart.jsx";
+import Loading from "./Components/Loading.jsx";
+import { Login } from "./Components/Login.jsx";
+import { getProfile, getProducts } from "./config/shopApi";
+export const AppContext = React.createContext();
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState(
+    JSON.parse(localStorage.getItem("cart")) || []
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const products = await getProducts();
+      console.log(products);
+      if (products) {
+        setProducts(products);
+      }
+      const storedEmail = localStorage.getItem("email");
+      if (storedEmail) {
+        const apiKey = localStorage.getItem("apiKey");
+        if (apiKey) {
+          getProfile().then(async (data) => {
+            if (data) {
+              const username = data.emailId.name;
+              setLoading(false);
+              toast.success(`Chào mừng bạn đã quay trở lại ${username}`);
+              setEmail(storedEmail);
+            }
+          });
+        } else {
+          Login(setLoading, setEmail);
+        }
+      } else {
+        Login(setLoading, setEmail);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleAddToCart = async (product) => {
+    setLoading(true);
+    const updatedCart = [...cart];
+    const existingProduct = updatedCart.find(
+      (item) => item._id === product._id
+    );
+
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+      existingProduct.left -= 1;
+    } else {
+      updatedCart.push({ ...product, quantity: 1, left: product.quantity - 1 });
+    }
+    setCart(updatedCart);
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+    setLoading(false);
+    toast.success("Sản phẩm đã được thêm vào giỏ hàng!");
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ cart, products, setCart, setLoading, setProducts }}
+    >
+      <div className="shop">
+        <div className="shop-inner">
+          <h1>Welcome to Shop!</h1>
+          {loading && <Loading />}
+
+          {email !== "" && (
+            <>
+              <ProductList handleAddToCart={handleAddToCart} />
+              <Cart />
+            </>
+          )}
+        </div>
+        <ToastContainer />
+      </div>
+    </AppContext.Provider>
+  );
+}
+
+export default App;
---
+++ Day_43/src/Component/cart.jsx
@@ -0,0 +1,119 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+import { getProducts, postOrder } from "../config/shopApi";
+import { toast } from "react-toastify";
+export default function Cart() {
+  const { cart, setCart } = useContext(AppContext);
+  const { products, setProducts } = useContext(AppContext);
+  const { setLoading } = useContext(AppContext);
+  const handleChange = (e, productId) => {
+    const updatedCart = [...cart];
+    const newQuantity = parseInt(e.target.value);
+    updatedCart.map((productCart) => {
+      if (productCart._id === productId) {
+        const oldQuantity = productCart.quantity;
+        productCart.quantity = newQuantity;
+        if (newQuantity > oldQuantity) {
+          productCart.left -= 1;
+        } else {
+          productCart.left += 1;
+        }
+      }
+    });
+
+    setCart(updatedCart);
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+  };
+
+  const handlePayment = () => {
+    setLoading(true);
+    const orderData = cart.map((item) => ({
+      productId: item._id,
+      quantity: item.quantity,
+    }));
+    postOrder(orderData).then(async (data) => {
+      if (data) {
+        setLoading(false);
+        toast.info("Thanh toán thành công");
+        localStorage.removeItem("cart");
+        setCart([]);
+        const newProducts = await getProducts();
+        setProducts(newProducts);
+      }
+    });
+  };
+
+  const TotalQuantity = () => {
+    const totalQuantity = cart.reduce(
+      (total, item) => total + item.quantity,
+      0
+    );
+    return totalQuantity;
+  };
+
+  const TotalPrice = () => {
+    const totalPrice = cart.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+    return totalPrice;
+  };
+  return cart.length > 0 ? (
+    <>
+      <table className="cart">
+        <thead>
+          <tr>
+            <th style={{ width: "30%" }}>Hình ảnh</th>
+            <th style={{ width: "20%" }}>Tên sản phẩm</th>
+            <th style={{ width: "20%" }}>Số lượng</th>
+            <th style={{ width: "10%" }}>Còn lại</th>
+            <th style={{ width: "20%" }}>Giá tiền</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map(({ _id, name, quantity, price, image, left }) => {
+            return (
+              <tr key={_id}>
+                <td>
+                  <img src={image} alt="" />
+                </td>
+                <td>{name}</td>
+                <td>
+                  <input
+                    type="number"
+                    value={quantity}
+                    onChange={(e) => {
+                      handleChange(e, _id);
+                    }}
+                    min={1}
+                  />
+                </td>
+                <td>{left}</td>
+                <td>{price * quantity}$</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={2}>Total</td>
+            <td colSpan={2}>{TotalQuantity()}</td>
+            <td>{TotalPrice()}$</td>
+          </tr>
+        </tfoot>
+      </table>
+      <button
+        type="button"
+        className="btn"
+        style={{ marginRight: "auto", fontSize: "2rem" }}
+        onClick={handlePayment}
+      >
+        Thanh Toán
+      </button>
+    </>
+  ) : (
+    <div style={{ marginTop: "20px", color: "#fff" }}>
+      Chưa có sản phẩm nào trong giỏ hàng
+    </div>
+  );
+}
